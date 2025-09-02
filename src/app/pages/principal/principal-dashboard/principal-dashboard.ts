0709ce7661ed8d86dfc401a4ba66bf240ebdef59
putrirealini <putrirealini@gmail.com>
2025-09-02 17:34:35 +0800

    Adding folder pager and teacher principal component

---
+++ src/app/pages/principal/principal-dashboard/principal-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-dashboard',
+  imports: [],
+  templateUrl: './principal-dashboard.html',
+  styleUrl: './principal-dashboard.css'
+})
+export class PrincipalDashboard {
+
+}
