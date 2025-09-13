2ec563bc7f28ba2eb317b7ac8925f124a1a835eb
putrirealini <putrirealini@gmail.com>
2025-09-13 20:38:43 +0800

    feat: add register page UI and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login'; 
+import { Register } from './pages/register/register';
+import { TeacherDashboard } from './pages/teacher/teacher-dashboard/teacher-dashboard';
 
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register},
+    { path: 'teacher-dashboard', component: TeacherDashboard},
+];
